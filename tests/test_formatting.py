69d2ebf7bd703ea688a08a4d44f9fdaaa6000b5b
MisanthropicBit <alexander.asp.bock@gmail.com>
2017-04-12 09:19:41 -0500

    Define a preset field ordering and remove PYTHONHASHSEED stuff for now

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -4,22 +4,26 @@ import bibpy
 import pytest
 
 
+# Use a predefined order until we can fix arbitrary ordering
+_PRESET_ORDER = ['author', 'title', 'year', 'month', 'institution']
+
+
 @pytest.fixture
 def test_entries():
     return bibpy.read_file('tests/data/simple_1.bib', 'biblatex').entries
 
 
-@pytest.mark.skip
 def test_formatting(test_entries):
     print(test_entries[0].format())
 
-    assert test_entries[0].format(align=True, indent='    ', order=[]) ==\
+    assert test_entries[0].format(align=True, indent='    ',
+                                  order=_PRESET_ORDER) ==\
         """@article{test,
-    month       = {4},
+    author      = {James Conway and Archer Sterling},
     title       = {1337 Hacker},
-    institution = {Office of Information Management {and} Communications},
     year        = {2010},
-    author      = {James Conway and Archer Sterling}
+    month       = {4},
+    institution = {Office of Information Management {and} Communications}
 }"""
 
     assert test_entries[1].format(align=True, indent='    ', order=[]) ==\
@@ -28,19 +32,15 @@ def test_formatting(test_entries):
 }"""
 
 
-@pytest.mark.skip
-def test_align(test_entries, monkeypatch):
-    # Set PYTHONHASHSEED to zero for Python 3+ to ensure predictable ordering
-    # of Python's dictionary
-    monkeypatch.setenv('PYTHONHASHSEED', 0)
-
-    assert test_entries[0].format(align=False, indent='    ', order=[]) ==\
+def test_align(test_entries):
+    assert test_entries[0].format(align=False, indent='    ',
+                                  order=_PRESET_ORDER) ==\
         """@article{test,
-    month = {4},
+    author = {James Conway and Archer Sterling},
     title = {1337 Hacker},
-    institution = {Office of Information Management {and} Communications},
     year = {2010},
-    author = {James Conway and Archer Sterling}
+    month = {4},
+    institution = {Office of Information Management {and} Communications}
 }"""
 
 
