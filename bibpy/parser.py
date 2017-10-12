d3dadc7841c64e402c8c79b5af367f6894c64919
MisanthropicBit <alexander.asp.bock@gmail.com>
2017-10-12 20:36:48 +0200

    Use importlib to make loading supported parsers dynamic

--- bibpy/parser.py
+++ bibpy/parser.py
@@ -2,18 +2,34 @@
 
 """Attempt to import a supported parser."""
 
-
-_ERR_MSG = "None of the supported parsers exist on this system "\
-           "(attempted: {0})".format(", ".join('funcparserlib, pyparsing'))
+import importlib
 
 
-try:
-    # Try funcparserlib first because the current implementation is fastest
-    from bibpy.parsers.fpl_parser import parse, parse_file, parse_date,\
-        parse_string_expr, parse_braced_expr, parse_query
-except ImportError:
+# Try funcparserlib first because the current implementation is fastest
+_parser_abbreviations = [
+    ('funcparserlib', 'fpl'),
+    ('pyparsing', 'pp'),
+]
+
+for parserlib, abbrev in _parser_abbreviations:
     try:
-        from bibpy.parsers.pp_parser import parse, parse_file, parse_date,\
-            parse_string_expr, parse_braced_string_expr
+        pmod = importlib.import_module('bibpy.parsers.{0}_parser'
+                                       .format(abbrev))
+
+        # Set parser functions
+        parse = pmod.parse
+        parse_file = pmod.parse_file
+        parse_date = pmod.parse_date
+        parse_string_expr = pmod.parse_string_expr
+        parse_braced_expr = pmod.parse_braced_expr
+        parse_query = pmod.parse_query
+
+        break
+    except AttributeError:
+        pass  # Ignore parser if not all functions are implemented
     except ImportError:
-        raise ImportError(_ERR_MSG)
+        pass
+else:
+    raise ImportError("None of the supported parsers exist on this system "
+                      "(attempted: {0})"
+                      .format(", ".join(p for p, _ in _parser_abbreviations)))
