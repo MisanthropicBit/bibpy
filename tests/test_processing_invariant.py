201eb8503d56018e9008f11514428aa31432d25e
MisanthropicBit <albo.developer@gmail.com>
2018-02-12 22:45:03 +0100

    Remove explicit format

--- tests/test_processing_invariant.py
+++ tests/test_processing_invariant.py
@@ -20,8 +20,8 @@ def test_string():
 
 
 def test_processing_invariant(test_string):
-    entry = bibpy.read_string(test_string, 'relaxed',
-                              postprocess=True).entries[0]
+    entry = bibpy.read_string(test_string, postprocess=True,
+                              remove_braces=True).entries[0]
 
     assert entry.author == ['James Conway', 'Archer Sterling']
     assert entry.xdata == ['key1', 'key2', 'key3', 'key4', 'key5']
@@ -33,8 +33,8 @@ def test_processing_invariant(test_string):
     assert entry.msg == '"Part of " # var # " string"'
     assert entry.foreword == ['Jan Leo and the Editors']
 
-    entry = bibpy.read_string(bibpy.write_string([entry]), 'relaxed',
-                              postprocess=True).entries[0]
+    entry = bibpy.read_string(bibpy.write_string([entry]), postprocess=True,
+                              remove_braces=True).entries[0]
 
     assert entry.xdata == ['key1', 'key2', 'key3', 'key4', 'key5']
     assert entry.urldate == bibpy.date.DateRange.fromstring('2017-01-14')
