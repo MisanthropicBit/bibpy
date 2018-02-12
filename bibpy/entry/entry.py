8bd9781a87338e77925e86cc5fbf2e32df51687c
MisanthropicBit <albo.developer@gmail.com>
2018-02-12 22:22:37 +0100

    Use an ordered dict to maintain field order
    When reading files, we want to maintain the same order as the fields are
    listed in the file, so that when we output the entry again the same
    field order is maintained. This would not be the case with an ordinary
    dictionary (due to PYTHONHASHSEED).

--- bibpy/entry/entry.py
+++ bibpy/entry/entry.py
@@ -6,12 +6,11 @@ import bibpy.compat
 import bibpy.entries
 import bibpy.error
 import bibpy.fields
-import bibpy.preprocess
 import bibpy.requirements
-from bibpy.entry import base
+import collections
+import itertools
 
 
-# TODO: Use collections.OrderedDict for fields(?) (kwargs are not ordered)
 class Entry(base.BaseEntry):
     """Represents an entry in a bib(la)tex file."""
 
@@ -27,12 +26,18 @@ class Entry(base.BaseEntry):
         Using keyword arguments are not guaranteed to keep the same ordering
         until Python 3.6 (also see PEP 468).
 
-        for field, value in fields.items():
+        """
+        # We use an ordered dict here to maintain the same order as the fields
+        # are listed in files
+        self._fields = collections.OrderedDict()
+        self._bibtype = bibtype
+        self._bibkey = bibkey
+
+        for field, value in itertools.chain(fields, kw_fields.items()):
             setattr(self, field, value)
 
-    # TODO: How can we test this across versions?
     def format(self, align=True, indent='    ', order=[], surround='{}',
-               **kwargs):  # pragma: no cover
+               **kwargs):
         """Format and return the entry as a string.
 
         'align' aligns the equal signs of all fields.
@@ -50,27 +55,33 @@ class Entry(base.BaseEntry):
 
         if order:
             if type(order) is bool:
-                fields = sorted(bibpy.preprocess.preprocess(self, self.fields))
+                # Sort alphabetically
+                fields = sorted(preprocess(self, self.fields, **kwargs))
             elif type(order) is list:
+                # Sort according to the specified order
                 order = [o for o in order if getattr(self, o, None)]
-                fields = list(bibpy.preprocess.preprocess(self, order)) +\
-                    list(bibpy.preprocess.preprocess(self,
-                                                     [f for f in self.fields
-                                                      if f not in order]))
+                ordered_fields = list(preprocess(self, order, **kwargs))
+                other_fields = list(preprocess(self, [f for f in self.fields
+                                                      if f not in order],
+                                               **kwargs))
+
+                fields = ordered_fields + other_fields
+
             else:
                 raise ValueError("order must be either a bool or a list, not "
                                  "'{0}'".format(type(order)))
         else:
-            for field, value in bibpy.preprocess.preprocess(self, self.fields):
+            # Otherwise, just preprocess all fields in their current order
+            for field, value in preprocess(self, self.fields, **kwargs):
                 fields.append((field, value))
 
-        fields = (",\n".join([bibpy.compat.u("{0}{1}{2} = {3}{4}{5}")
-                              .format(indent, field,
-                                      ' ' * (i - len(field)),
-                                      surround[0], value, surround[1])
-                              for field, value in fields]))
+        formatted_fields =\
+            [u("{0}{1}{2} = {3}{4}{5}")
+             .format(indent, field, ' ' * (i - len(field)),
+                     surround[0], value, surround[1])
+             for field, value in fields]
 
-        return entry_start + fields + "\n}"
+        return entry_start + ",\n".join(formatted_fields) + "\n}"
 
     @property
     def bibtype(self):
@@ -97,7 +108,7 @@ class Entry(base.BaseEntry):
         Active fields are fields that are not None or empty strings.
 
         """
-        return list(self._fields)
+        return self._fields.keys()
 
     @property
     def extra_fields(self):
@@ -107,7 +118,7 @@ class Entry(base.BaseEntry):
         underscore.
 
         """
-        return [prop for prop in vars(self) if not prop.startswith('_')]
+        return [ef for ef in self.fields if ef not in bibpy.fields.all]
 
     def get(self, name, default=None):
         """Return the value for name or default if the name does not exist."""
@@ -115,7 +126,7 @@ class Entry(base.BaseEntry):
 
     def aliases(self, format):
         """Return any aliases of this entry."""
-        return bibpy.entries.aliases(self.entry_type, format)
+        return bibpy.entries.aliases(self.bibtype, format)
 
     def valid(self, format):
         """Return True if all required fields are present, False otherwise."""
@@ -144,12 +155,11 @@ class Entry(base.BaseEntry):
             setattr(self, field, None)
 
     def __eq__(self, other):
-        """Entries are equal if their fields and values match."""
+        """Entries are equal if their types, keys, fields and values match."""
         if not isinstance(other, Entry):
             return False
 
-        if self.bibtype != other.bibtype or\
-                self.bibkey != other.bibkey:
+        if self.bibtype != other.bibtype or self.bibkey != other.bibkey:
             return False
 
         if set(self.fields) == set(other.fields):
@@ -172,13 +182,12 @@ class Entry(base.BaseEntry):
     def __setattr__(self, name, value):
         super(Entry, self).__setattr__(name, value)
 
-        if not name.startswith('_'):
-            if name not in Entry._locked_fields:
-                if value is None or value == '':
-                    if name in self._fields:
-                        self._fields.remove(name)
-                else:
-                    self._fields.add(name)
+        if not name.startswith('_') and name not in Entry._locked_fields:
+            if value is None or value == '':
+                if name in self._fields:
+                    self._fields.pop(name)
+            else:
+                self._fields[name] = None
 
     def __setitem__(self, key, value):
         setattr(self, key, value)
@@ -196,8 +205,7 @@ class Entry(base.BaseEntry):
         return len(self.fields)
 
     def __repr__(self):
-        return "Entry(type={0}, key={1})".format(self.bibtype,
-                                                 self.bibkey)
+        return "Entry(type={0}, key={1})".format(self.bibtype, self.bibkey)
 
 
 def autoproperty(name, getter=True, setter=True, prefix='_'):
