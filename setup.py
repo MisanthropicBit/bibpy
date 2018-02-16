e47561e18f593c3c5a57f58ed9987f2d5babc747
MisanthropicBit <alexander.asp.bock@gmail.com>
2018-02-16 09:10:35 +0100

    Remove 3.4 from Travis build and setup.py, add 3.6

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ distutils.core.setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6',
     ]
 )
