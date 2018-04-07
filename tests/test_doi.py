a3948348e35a065c61641c4223ce3485bf44a25b
MisanthropicBit <albo.developer@gmail.com>
2018-04-07 11:00:12 +0200

    Add doi postprocessing test

--- tests/test_doi.py
+++ tests/test_doi.py
@@ -33,6 +33,29 @@ def test_doi():
 
 
 @not_on_travis
+def test_doi_postprocess():
+    with vcr.use_cassette('fixtures/vcr_cassettes/doi.yaml'):
+        doi = '10.1145/1015530.1015557'
+        entry = bibpy.doi.retrieve(doi, postprocess=True)
+
+        assert len(entry.fields) == 11
+        assert entry.bibtype == 'article'
+        assert entry.bibkey == 'Hancock_2004'
+        assert entry.author == ['Jeffrey T. Hancock']
+        assert entry.title == '{LOL}'
+        assert entry.journal == 'interactions'
+        assert entry.volume == 11
+        assert entry.number == 5
+        assert entry.month == 'September'
+        assert entry.year == 2004
+        assert entry.pages == '57'
+        assert entry.url == 'https://doi.org/10.1145%2F1015530.1015557'
+        assert entry.publisher ==\
+            ['Association for Computing Machinery ({ACM})']
+        assert entry.doi == doi
+
+
+@not_on_travis
 def test_doi_raw():
     with vcr.use_cassette('fixtures/vcr_cassettes/doi.yaml'):
         doi = '10.1145/1015530.1015557'
