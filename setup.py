56d151b7285a43b8946b4ea8da382fd97803b751
MisanthropicBit <albo.developer@gmail.com>
2019-09-17 21:12:30 +0200

    Add support for 3.4 and 3.7

--- setup.py
+++ setup.py
@@ -33,8 +33,10 @@ distutils.core.setup(
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
         'Programming Language :: Python :: 3.5'
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: PyPy'
     ]
 )
