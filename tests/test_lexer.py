ae10d90b8bb98d572d670af711fd7eabdf472a6a
MisanthropicBit <albo.developer@gmail.com>
2020-05-13 12:00:11 +0200

    Prefer single-quoted strings

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -72,10 +72,10 @@ This is another"""
 
 
 def test_namelist_lexer():
-    test1 = "T. Ohtsuki and H. Mori and T. Kashiwabara and T. Fujisawa"
-    test2 = "T. Ohtsuki and H. Moriand and T. Kashiwabara and T. Fujisawa"
-    test3 = "L. {Sunil Chandran} and C. R. Subramanian"
-    test4 = "L. {Sunil Chandran} {and } C. R. Subramanian"
+    test1 = 'T. Ohtsuki and H. Mori and T. Kashiwabara and T. Fujisawa'
+    test2 = 'T. Ohtsuki and H. Moriand and T. Kashiwabara and T. Fujisawa'
+    test3 = 'L. {Sunil Chandran} and C. R. Subramanian'
+    test4 = 'L. {Sunil Chandran} {and } C. R. Subramanian'
 
     assert list(NamelistLexer().lex(test1)) ==\
         ['T. Ohtsuki', 'H. Mori', 'T. Kashiwabara', 'T. Fujisawa']
